85a5d5462e62948b80570b2bba6b80ff975da4b9
Hackuuur <kothadiyaharsh705@gmail.com>
2024-01-20 12:06:49 +0530

    .

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -69,8 +69,8 @@ const ThankYouPage = async ({
       <div>
         <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24'>
           <div className='lg:col-start-2'>
-            <p className='text-sm font-medium text-blue-600'>
-              Order successful
+            <p className='text-lg font-medium text-green-600'>
+              Order successful !
             </p>
             <h1 className='mt-2 text-4xl font-bold tracking-tight text-gray-300 sm:text-5xl'>
               Thanks for ordering
