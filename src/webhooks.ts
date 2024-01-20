2827f3449dfb358a3e504e5fc7236968057698fe
Harsh <108812699+Hackuuur@users.noreply.github.com>
2024-01-20 14:07:23 +0530

    Update webhooks.ts

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -99,7 +99,7 @@ export const stripeWebhookHandler = async (
     // send receipt
     try {
       const data = await resend.emails.send({
-        from: 'DigitalHippo <hello@joshtriedcoding.com>',
+        from: 'DigitalHippo <hello@anistream.world>',
         to: [user.email],
         subject:
           'Thanks for your order! This is your receipt.',
