a1ffe4502236a3eacf7d683cd05e00f82ec6e095
Harsh <108812699+Hackuuur@users.noreply.github.com>
2024-01-20 14:20:03 +0530

    Update payment-router.ts

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -61,21 +61,20 @@ export const paymentRouter = router({
         },
       })
 
-      try {
-        const stripeSession =
-          await stripe.checkout.sessions.create({
-            success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
-            cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
-            payment_method_types: ['card', 'paypal'],
-            mode: 'payment',
-            metadata: {
-              userId: user.id,
-              orderId: order.id,
-            },
-            line_items,
-          })
-
-        return { url: stripeSession.url }
+       try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ["card"],
+          mode: "payment",
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+            
+          },
+          line_items,
+        });
+        return { url: stripeSession.url };
       } catch (err) {
         return { url: null }
       }
