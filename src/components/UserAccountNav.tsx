1e049ad8a88cbc64a8ab8b566390647e309b8dec
Harsh <108812699+Hackuuur@users.noreply.github.com>
2024-01-20 19:58:48 +0530

    Update UserAccountNav.tsx

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+
+import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuItem,
-} from "@radix-ui/react-dropdown-menu";
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { User } from "@/payload-types";
@@ -14,33 +18,46 @@ import { useAuth } from "@/hooks/use-auth";
 const UserAccountNav = ({ user }: { user: User }) => {
   const { signOut } = useAuth();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className="overflow-visible  border-black ">
+    <Drawer>
+      <DrawerTrigger asChild>
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" className=" rounded-full " />
+          <AvatarImage
+            src="https://github.com/shadcn.png"
+            className=" rounded-full "
+          />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className=" p-2 text-black bg-white w-60 rounded-sm mt-2 py-2"
-        align="end"
-      >
-        <div className="flex items-center justify-start gap-2 ">
-          <div className=" p-2 flex flex-col space-y-0.5 leading-none">
-            <p className=" font-medium text-sm  ">{user.email}</p>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm ">
+          <DrawerHeader>
+            <Avatar>
+              <AvatarImage
+                src="https:/z/github.com/shadcn.png"
+                className=" rounded-full "
+              />
+              <AvatarFallback>AV</AvatarFallback>
+            </Avatar>
+          </DrawerHeader>
+          <div className="flex items-center justify-start gap-2 ">
+            <div className=" p-2 flex flex-col space-y-0.5 leading-none">
+              <p className=" font-medium text-sm  ">{user.email}</p>
+            </div>
           </div>
-        </div>
-
-        <DropdownMenuItem asChild>
           <Link href="/creator" className="p-2 backdrop-blur-md bg-opacity-50">
             Creator Dashboard
           </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={signOut} className="  cursor-pointer p-2 ">
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DrawerFooter>
+            <Button onClick={signOut} className="  cursor-pointer p-2 ">
+              Log out
+            </Button>
+            <DrawerClose asChild>
+              <Button variant="outline">x</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
