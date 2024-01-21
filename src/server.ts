8be4d81013248256f565f3f1080c0e1041f8e717
Hackuuur <kothadiyaharsh705@gmail.com>
2024-01-21 10:18:37 +0530

    .

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
-import { nextApp, nextHandler } from "./next-util";
+import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
