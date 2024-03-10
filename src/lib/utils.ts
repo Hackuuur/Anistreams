4efac197315ff347e778081d18805e6de935bbe3
deefame <koh803074@gmail.com>
2024-03-10 11:54:17 +0530

    .

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
@@ -22,4 +23,46 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice)
+}
+export function constructMetadata({
+  title = 'AniStream - Online Free Anime Streaming and manga MarketPlace',
+  description = 'Online Anime Streaming platform and Marketplace for manga purchase and sell ',
+  image = '/Logo.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@Harsh',
+    },
+    icons,
+    metadataBase: new URL('http://localhost:3000'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
